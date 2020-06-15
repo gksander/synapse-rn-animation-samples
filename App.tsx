8218cf68514f8e60a748251dd410b3f06001d2a7
Grant Sander <gksander93@gmail.com>
2020-06-15 16:02:39 -0700

    Add some icons

--- App.tsx
+++ App.tsx
@@ -1,19 +1,35 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {ToastsView} from "./views/Toasts.view";
 import {TestView} from "./views/Test.view";
+import {MaterialCommunityIcons} from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
 const App: React.FC = () => (
-  <NavigationContainer>
-    <Tab.Navigator>
-      <Tab.Screen name="Toasts" component={ToastsView} />
-      <Tab.Screen name="Testing" component={TestView} />
-    </Tab.Navigator>
-  </NavigationContainer>
+	<NavigationContainer>
+		<Tab.Navigator
+			screenOptions={({route}) => ({
+				tabBarIcon: ({focused, color, size}) => {
+					switch (route.name) {
+						case 'Toasts':
+							return <MaterialCommunityIcons name="toaster-oven" size={size} color={color}/>;
+						default:
+							return <MaterialCommunityIcons name="access-point" size={size} color={color}/>;
+					}
+				},
+			})}
+			tabBarOptions={{
+				activeTintColor: 'tomato',
+				inactiveTintColor: 'gray',
+			}}
+		>
+			<Tab.Screen name="Toasts" component={ToastsView}/>
+			<Tab.Screen name="Testing" component={TestView}/>
+		</Tab.Navigator>
+	</NavigationContainer>
 )
 
 export default App;
